134f4023e1581d7a9e6b0dd081f5d01657d85ea8
Kushal Shrestha <shresthakushal097@gmail.com>
2026-01-28 22:18:15 +0545

    Trying to complete the task

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Crud.Entity;
+using Crud.Data;
+using Crud.DTO;
+
+namespace Crud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApplicationDBContext dbContext;
+        public StudentController(ApplicationDBContext dBContext)
+        {
+            dbContext = dBContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var students = dbContext.Students.ToList();
+            var studentDTO = new List<StudentDTO>();
+            foreach (var student in students)
+            {
+                studentDTO.Add(new StudentDTO
+                {
+                    Id = student.Id,
+                    Name = student.Name,
+                    Semester_id = student.Semester_id
+                });
+            }
+            return Ok(studentDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var student = dbContext.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            var studentDTO = new StudentDTO
+            {
+                Id = student.Id,
+                Name = student.Name,
+                Semester_id = student.Semester_id
+            };
+            return Ok(studentDTO);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] StudentDTO studentDto)
+        {
+            //map or convert DTO to Entity
+            var student = new Student
+            {
+                Id = Guid.NewGuid(),
+                Name = studentDto.Name,
+                Semester_id = studentDto.Semester_id
+            };
+            dbContext.Students.Add(student);
+            dbContext.SaveChanges();
+
+            var createdStudentDTO = new StudentDTO
+            {
+                Id = student.Id,
+                Name = student.Name,
+                Semester_id = student.Semester_id
+            };
+            return CreatedAtAction(nameof(GetById), new { id = student.Id }, createdStudentDTO);
+        }
+    }
+}
---
+++ Controllers/SubjectController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Crud.Entity;
+using Crud.Data;
+using Crud.DTO;
+
+namespace Crud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        private readonly ApplicationDBContext dbContext;
+        public SubjectController(ApplicationDBContext dBContext)
+        {
+            dbContext = dBContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var subjects = dbContext.Subjects.ToList();
+            var subjectDTO = new List<SubjectDTO>();
+            foreach (var subject in subjects)
+            {
+                subjectDTO.Add(new SubjectDTO
+                {
+                    Id = subject.Id,
+                    Name = subject.Name,
+                    description = subject.description
+                });
+            }
+            return Ok(subjectDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var subject = dbContext.Subjects.Find(id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            var subjectDTO = new SubjectDTO
+            {
+                Id = subject.Id,
+                Name = subject.Name,
+                description = subject.description
+            };
+            return Ok(subjectDTO);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] SubjectDTO subjectDto)
+        {
+            //map or convert DTO to Entity
+            var subject = new Subject
+            {
+                Id = Guid.NewGuid(),
+                Name = subjectDto.Name,
+                description = subjectDto.description
+            };
+            dbContext.Subjects.Add(subject);
+            dbContext.SaveChanges();
+
+            var createdSubjectDTO = new SubjectDTO
+            {
+                Id = subject.Id,
+                Name = subject.Name,
+                description = subject.description
+            };
+            return CreatedAtAction(nameof(GetById), new { id = subject.Id }, createdSubjectDTO);
+        }
+    }
+}
